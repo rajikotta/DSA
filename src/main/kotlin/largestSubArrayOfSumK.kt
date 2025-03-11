934abc0ca3c2c58b1ddd1e27a1bfe882d0cd9948
Raji Chithrangathan <raji.chithrangathan@goldenscent.com>
2025-03-11 10:57:01 +0400

    largestSubArrayOfSumKWithMap

---
+++ src/main/kotlin/largestSubArrayOfSumK.kt
@@ -0,0 +1,21 @@
+fun largestSubArrayOfSumK(array: IntArray, k: Int): Int {
+    var leng = 0
+
+    var i = 0
+    var sum = 0
+    for (j in 0..array.lastIndex) {
+
+        sum += array[j]
+
+        if (sum == k)
+            leng = maxOf(leng, j - i + 1)
+        else if (sum > k) {
+            while (sum > k && i <= j) {
+                sum -= array[i]
+                i++
+            }
+        }
+    }
+    return leng
+
+}
